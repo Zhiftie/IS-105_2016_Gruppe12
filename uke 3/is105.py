ba134c3fa1781ba3aca0068cf1c5b965d61bf177
Zhiftie <sghaugeland@live.com>
2016-01-25 12:49:20 +0100

    Ryddet i master

--- uke 3/is105.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-'''
-Modul med diverse funksjoner brukt i IS-105 våren 2016
-'''
-def add(x,y):
-    return x + y
--- uke 3/lene_is105.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-'''
-Module med diverse funksjoner brukt i IS-105 våren 2016
-'''
-def add(x,y):
-    return x + y
-
