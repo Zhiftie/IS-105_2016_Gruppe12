ccb6c798b370bb11cdf0772e008d84731997ecd6
martinnenseth <martinnenseth@gmail.com>
2016-01-25 12:30:26 +0100

    mm

--- uke 3/test_add_is105.py
+++ uke 3/test_add_is105.py
@@ -5,11 +5,11 @@ class TestAdd(unittest.TestCase):
     def setUp(self):
         pass
     def test_numbers_100_7(self):
-<<<<<<< HEAD
+
         self.assertEqual(add(100,7), 107)
-=======
+
         self.assertEqual(add(10,7), 17)
->>>>>>> cacfdd8db894d42cad203152080c491c316f566b
+
         
 
     
