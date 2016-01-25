074f826236909f01de7a6d93afdf916271d76c89
martinnenseth <martinnenseth@gmail.com>
2016-01-25 12:26:50 +0100

    changes

--- uke 3/stein_saks_papir.py
+++ uke 3/stein_saks_papir.py
@@ -1,40 +1,22 @@
-import string
 import random
 print "Velg STEIN, SAKS eller PAPIR"
 
-<<<<<<< HEAD
 
 choices = ["STEIN", "SAKS", "PAPIR"]
 choices1 = ["STEIN", "SAKS", "PAPIR"]
 
+
 p1 = "player one rolls:"
 p2 = "player two rolls:"
+
 print p1, (choices[random.randint(0,2)])
+choices = (choices[random.randint(0,2)])
 print "VS"
+choices1 = (choices1[random.randint(0,2)])
 print p2, (choices1[random.randint(0,2)])
 
-
-
-
-
-
-
-
-=======
-pone = raw_input(' ')
-ptwo = raw_input(' ')
-status = "' '"
-
-def cast():
-    print "Player 1 brukte %s, Player 2 brukte %s" % (pone, ptwo)
-    check(pone, ptwo)
-
-def check(pone,ptwo):
-    if pone == ptwo:
-        status = "Draw"
-    #else pone != ptwo:
-        
+if choices == choices1:
+    print "Draw!"
     
-        
-    
->>>>>>> cacfdd8db894d42cad203152080c491c316f566b
+
+ 
