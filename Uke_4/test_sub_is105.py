8ddfcc70823ef91ce8e15af333f8de4488485dee
Tomma252 <Tomma@thomass-macbook.uia.no>
2016-01-29 11:53:09 +0100

    Oppdatering

---
+++ Uke_4/test_sub_is105.py
@@ -0,0 +1,12 @@
+import unittest 
+from Tomma_is105 import sub
+
+class TestSub (unittest.TestCase):
+    def setUp(self):
+        pass
+    def test_numbers_2_1(self):
+            self.assertEqual(sub(2,1), 1)
+            
+if __name__ == '__main__':
+    unittest.main()
+    
