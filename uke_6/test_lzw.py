645e4e6992b5a6210ca6610de8e6a47faff7d69c
kakelaks <guro_fotball@hotmail.com>
2016-02-11 14:40:01 +0100

    added tableTest

---
+++ uke_6/test_lzw.py
@@ -0,0 +1,19 @@
+import unittest
+from 121bV1.0 import compress()
+
+class TestCompress(unittest.TestCase):
+    def setUp(self):
+        pass
+    #def testMetode
+    #assert
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
+def test_table_entry_4(self):
+    self.asserEqual(code()[4], 'e')
+"""
+Sjekker om table 4s key er 'e'. 
+"""
