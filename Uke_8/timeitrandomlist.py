a48956aa20f3342d849f66ec14014b28a361dcd5
Zhiftie <sghaugeland@live.com>
2016-02-25 14:30:45 +0100

    timeit funksjon for search fast og search slow

---
+++ Uke_8/timeitrandomlist.py
@@ -0,0 +1,12 @@
+from randomlist import Search_s, Search_f
+if __name__ == "__main__":
+    """
+    timeit test for the search slow and search_fast function
+    """
+    import timeit
+    timer = timeit.Timer(Search_s)
+    result = timer.repeat(repeat = 100, number = 10)
+    print ("{:8.6f}".format(min(result)))
+    timer = timeit.Timer(Search_f)
+    result = timer.repeat(repeat = 100, number = 10)
+    print ("{:8.6f}".format(min(result)))
