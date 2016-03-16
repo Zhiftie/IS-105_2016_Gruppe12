e86467ed0c0b1ee985209d4c9c5092c291e45dc2
Tomma252 <Tomma@thomass-macbook.uia.no>
2016-03-16 10:26:13 +0100

    Siste revidering.

--- Uke_11/Fil_Funksjoner_Thomas.py
+++ Uke_11/Fil_Funksjoner_Thomas.py
@@ -2,24 +2,23 @@
 import os
 
 def newFile():
-if os.path.exists('location\filename.txt'):
-    print "File exists"
-
-else:
-   open("location\filename.txt", 'w')
+    if os.path.exists('location\filename.txt'):
+        print "File exists"
+    else:
+        open("location\filename.txt", 'w')
    
 # Søk i filen.
 def searchFile():
-with open('file.txt', 'r') as searchfile:
-    for line in searchfile:
-        if 'searchphrase' in line:
-            print line
+    with open('file.txt', 'r') as searchfile:
+        for line in searchfile:
+            if 'searchphrase' in line:
+                print line
             
 # Gi en fil et nytt navn.
 import os
 def renameFile():
-for fileName in os.listdir("."):
-    os.rename(fileName, fileName.replace("Filnavn", "NyttFilNavn"))
+    for fileName in os.listdir("."):
+        os.rename(fileName, fileName.replace("Filnavn", "NyttFilNavn"))
    
 
 # Slette en fil.
