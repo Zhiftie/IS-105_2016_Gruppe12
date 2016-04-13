f8f99af1475587720ee42b78a6392d50fa220e5c
Zhiftie <sghaugeland@live.com>
2016-04-13 11:38:50 +0200

    videre testing med client

--- Uke_15/Client.py
+++ Uke_15/Client.py
@@ -1,4 +1,4 @@
-l1 = { "hello" : t1, "test2" : t2 } #dict with all the commands, needs to be extended to cover all the different commands in state.py
+from test import test1 as t
 
 def t1():
     print "t1"
@@ -15,14 +15,34 @@ def listCmd():
     print list of all the different commands the player can use
     List of all the commands kept in state modul?
     """
+    for k in l1:
+        print k + " : Desc of key"
 def cmd(cmdW):
     """
     Use cmdW to find the appropriate command in the state module
     """
-def cmd(command): # Calls a cmd from dictionary with all the different commands based on the word entered as a parameter
-    l1[str(command)]()
+def cmd(): # Calls a cmd from dictionary with all the different commands based on the word entered as a parameter
+    #l1[raw_input("Insert Command >>> ")]()
+    inp = raw_input(">>> ")
+    if inp in l1:
+        l1[inp]()
+        return inp
+    else:
+        print "Command not found"
     
 def quit():
     """
     ends the process
     """
+    print "quit"
+
+l1 = { "hello" : t1, "test2" : t2, "quit" : quit, "ls" : listCmd  } #dict with all the commands, needs to be extended to cover all the different commands in state.py
+
+
+while True:
+    command = cmd()
+    if command is "quit":
+        break
+    
+    
+        
