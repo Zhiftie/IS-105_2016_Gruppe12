2e70dc1011b945000c69d6eb490893532476815b
martinnenseth <martinnenseth@gmail.com>
2016-06-03 14:50:19 +0200

    clean up ICA03 + readme

--- ICA03/test_code_flipped.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from code_flip import rev_table
-
-class TestCode(unittest.TestCase):
-    def setUp(self):
-        pass
-    def test_codeReverse(self):
-        self.assertEqual(rev_table()[' '], 7)
